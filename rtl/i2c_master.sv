db4fe48096b045ab5b04fe523b6c51932a4bba62
carnilin <lincarni2005@gmail.com>
2026-08-11 20:23:52 +0200

    Initial commit

---
+++ rtl/i2c_master.sv
@@ -0,0 +1,276 @@
+`timescale 1ns/1ns
+
+module i2c_master(
+    input logic clk, reset,
+
+    input logic wr_i2c,
+    input logic [2:0] cmd,
+    input logic [7:0] din,
+    input logic [15:0] dvsr,
+
+    output logic [7:0] dout,
+    output logic ack,
+    output logic ready,
+    output logic done_tick,
+
+    output tri scl,
+    inout tri sda
+    );
+
+    // COMMAND ENCODINGS
+    localparam START_CMD   = 3'b000;
+    localparam WR_CMD      = 3'b001;
+    localparam RD_CMD      = 3'b010;
+    localparam STOP_CMD    = 3'b011;
+    localparam RESTART_CMD = 3'b100;
+
+    // FSM STATES
+    typedef enum logic [3:0] {
+        idle,
+        start1,
+        start2,
+        hold,
+        data1,
+        data2,
+        data3,
+        data4,
+        data_end,
+        stop1,
+        stop2,
+        restart
+    } state_t;
+
+    // STATES AND REGISTERS
+    state_t state_reg, state_next;
+    logic [15:0] c_reg, c_next;
+    logic [15:0] qutr, half;
+    logic [8:0] tx_reg, tx_next;
+    logic [8:0] rx_reg, rx_next;
+    logic [2:0] cmd_reg, cmd_next;
+    logic [3:0] bit_reg, bit_next;
+    logic sda_out, scl_out, sda_reg, scl_reg, data_phase;
+    logic done_tick_i, ready_i;
+    logic into, nack;
+
+    // SDA, SCL BUFFER
+    always_ff @(posedge clk, posedge reset)
+        if (reset) begin
+            sda_reg <= 1'b1;
+            scl_reg <= 1'b1;
+        end
+        else begin
+            sda_reg <= sda_out;
+            scl_reg <= scl_out;
+        end
+
+    // SCL IS HIGH WHEN NOT DRIVEN
+    // DUE TO PULL-UP RESISTOR
+    assign scl = (scl_reg) ? 1'bz : 1'b0;
+
+    // "INTO" SIGNAL HIGH WHEN DATA BEING
+    // WRITTEN INTO MASTER
+    assign into = (data_phase && cmd_reg == RD_CMD && bit_reg < 8) || (data_phase && cmd_reg == WR_CMD && bit_reg == 8);
+
+    // HIGH WHEN NOT DRIVEN, RELEASE WHEN SLAVE
+    // WRITING INTO MASTER
+    assign sda = (into || sda_reg) ? 1'bz : 1'b0;
+
+    assign dout = rx_reg[8:1];
+    assign ack  = rx_reg[0];
+
+    // HARD WIRED TO ZERO, MIGHT BE A PROBLEM
+    assign nack = 1'b0;
+
+    always_ff @(posedge clk, posedge reset)
+        if (reset) begin
+            state_reg <= idle;
+            c_reg     <= 0;
+            bit_reg   <= 0;
+            cmd_reg   <= 0;
+            tx_reg    <= 0;
+            rx_reg    <= 0;
+        end
+        else begin
+            state_reg <= state_next;
+            c_reg     <= c_next;
+            bit_reg   <= bit_next;
+            cmd_reg   <= cmd_next;
+            tx_reg    <= tx_next;
+            rx_reg    <= rx_next;
+        end
+
+    // QUARTER AND HALF I2C CLK CYCLES
+    assign qutr = dvsr;
+    assign half = {qutr[14:0], 1'b0};
+
+    always_comb begin
+        state_next  = state_reg;
+        c_next      = c_reg + 1;
+        bit_next    = bit_reg;
+        tx_next     = tx_reg;
+        rx_next     = rx_reg;
+        cmd_next    = cmd_reg;
+        done_tick_i = 1'b0;
+        ready_i     = 1'b0;
+            
+        // BOTH HIGH BY DEFAULT
+        scl_out     = 1'b1;
+        sda_out     = 1'b1;
+
+        data_phase  = 1'b0;
+
+        case (state_reg)
+            idle: begin
+                ready_i = 1'b1;
+                if (wr_i2c && cmd == START_CMD) begin
+                    state_next = start1;
+                    c_next = 0;
+                end
+            end
+
+            // ======================================
+
+            start1: begin
+                sda_out = 1'b0;
+                if (c_reg == half) begin
+                    c_next = 0;
+                    state_next = start2;
+                end
+            end
+
+            // ======================================
+
+            start2: begin
+                scl_out = 1'b0;
+                sda_out = 1'b0;
+                if (c_reg == half) begin
+                    c_next = 0;
+                    state_next = hold;
+                end
+            end
+
+            // ======================================
+
+            hold: begin
+                ready_i = 1'b1;
+                sda_out = 1'b0;
+                scl_out = 1'b0;
+                if (wr_i2c) begin
+                    cmd_next = cmd;
+                    c_next = 0;
+                    case (cmd)
+                        RESTART_CMD, START_CMD: begin
+                            state_next = restart;
+                        end
+                        STOP_CMD: begin
+                            state_next = stop1;
+                        end
+                        default: begin
+                            bit_next = 0;
+                            state_next = data1;
+                            tx_next = {din, nack};
+                        end
+                    endcase
+                end
+            end
+
+            // ======================================
+
+            data1: begin
+               scl_out = 1'b0; 
+               sda_out = tx_reg[8];
+               data_phase = 1'b1;
+               if (c_reg == qutr) begin
+                   c_next = 0;
+                   state_next = data2;
+               end
+            end
+
+            // ======================================
+
+            data2: begin
+                sda_out = tx_reg[8];
+                data_phase = 1'b1;
+                if (c_reg == qutr) begin
+                    c_next = 0;
+                    state_next = data3;
+                    rx_next = {rx_reg[7:0], sda};
+                end
+            end
+
+            // ======================================
+
+            data3: begin
+                sda_out = tx_reg[8];
+                data_phase = 1'b1;
+                if (c_reg == qutr) begin
+                    c_next = 0;
+                    state_next = data4;
+                end
+            end
+
+            // ======================================
+
+            data4: begin
+                scl_out = 1'b0;
+                sda_out = tx_reg[8];
+                data_phase = 1'b1;
+                if (c_reg == qutr) begin
+                    c_next = 0;
+                    if (bit_reg == 8) begin
+                        state_next = data_end;
+                        done_tick_i = 1'b1;
+                    end
+                    else begin
+                        tx_next = {tx_reg[7:0], 1'b0};
+                        bit_next = bit_reg + 1;
+                        state_next = data1;
+                    end
+                end
+            end
+
+            // ======================================
+
+            data_end: begin
+                sda_out = 1'b0;
+                scl_out = 1'b0;
+                if (c_reg == qutr) begin
+                    c_next = 0;
+                    state_next = hold;
+                end
+            end
+
+            // ======================================
+
+            restart: begin
+                if (c_reg == half) begin
+                    c_next = 0;
+                    state_next = start1;
+                end
+            end
+
+            // ======================================
+
+            stop1: begin
+                sda_out = 1'b0;
+                if (c_reg == half) begin
+                    c_next = 0;
+                    state_next = stop2;
+                end
+            end
+
+            // ======================================
+
+            // STOP2
+            default: begin 
+                if (c_reg == half) begin
+                    state_next = idle;
+                end
+            end
+        endcase
+
+        assign done_tick = done_tick_i;
+        assign ready = ready_i;
+    end
+
+endmodule
---
+++ rtl/obi_i2c.sv
@@ -0,0 +1,144 @@
+`timescale 1ns/1ns
+
+module obi_i2c (
+    input  logic clk_i,
+    input  logic rstn_i,
+
+    input  logic                    obi_areq_i,
+    output logic                    obi_agnt_o,
+    input  logic [ADDR_WIDTH-1:0]   obi_aaddr_i,
+    input  logic [DATA_WIDTH-1:0]   obi_awdata_i,
+
+    input  logic                    obi_awe_i,
+    input  logic [DATA_WIDTH/8-1:0] obi_abe_i,
+
+    output logic                    obi_rvalid_o,
+    input  logic                    obi_rready_i,
+    output logic [DATA_WIDTH-1:0]   obi_rdata_o,
+    output logic                    obi_rerr_o    // TIED OFF
+    );
+
+    // NOT A GLOBAL PARAMETER FOR SAFETY REASONS
+    localparam integer DATA_WIDTH = 32;
+    localparam integer ADDR_WIDTH = 32;
+
+    // REGISTER OFFSETS
+    localparam integer I2C_STATUS_REG = 0;
+    localparam integer I2C_DATA_REG   = 4;
+    localparam integer I2C_SPEED_REG  = 8;
+
+    // WRAPPER REGISTERS 
+    logic [DATA_WIDTH-1:0] i2c_status_reg; // read-only
+    logic [DATA_WIDTH-1:0] i2c_data_reg;
+    logic [DATA_WIDTH-1:0] i2c_speed_reg;
+
+    // OBI STATE FSM
+    typedef enum logic {
+        ADDR,
+        RESP
+    } obi_state_t;
+    obi_state_t state_reg, state_next;
+
+    always_ff @(posedge clk_i)
+        state_reg <= state_next;
+
+    logic obi_a_fire;
+    logic obi_r_fire;
+
+    assign obi_a_fire = obi_agnt_o   && obi_areq_i;
+    assign obi_r_fire = obi_rvalid_o && obi_rready_i;
+
+    always_comb begin
+        state_next = state_reg;
+        case (state_reg)
+            ADDR: begin
+                if (obi_a_fire)
+                    state_next = RESP;
+            end
+            RESP: begin
+                if (obi_r_fire)
+                    state_next = ADDR;
+            end
+        endcase
+    end
+
+    // OBI ERROR
+    assign obi_rerr_o = 1'b0;
+
+    // OBI ADDR PHASE
+    logic [DATA_WIDTH-1:0] data_mask;
+    assign data_mask = {{8{obi_abe_i[3]}}, {8{obi_abe_i[2]}}, {8{obi_abe_i[1]}}, {8{obi_abe_i[0]}}};
+
+    always_ff @(posedge clk_i) begin
+        if (obi_a_fire) begin
+            case ({28'b0, obi_aaddr_i[3:2], 2'b0})
+                I2C_STATUS_REG: begin
+                    if (~obi_awe_i)
+                        obi_rdata_o <= i2c_status_reg;
+                end
+                I2C_DATA_REG: begin
+                    if (obi_awe_i)
+                        i2c_data_reg <= obi_awdata_i & data_mask;
+                    else
+                        obi_rdata_o <= i2c_data_reg;
+                end
+                I2C_SPEED_REG: begin
+                    if (obi_awe_i)
+                        i2c_speed_reg <= obi_awdata_i & data_mask;
+                    else
+                        obi_rdata_o <= i2c_speed_reg;
+                end
+                default: ; // do nothing for unknown addresses
+            endcase
+        end
+    end
+
+    // OBI HANDSHAKE SIGNALS
+    assign obi_agnt_o   = (state_reg == ADDR);
+    assign obi_rvalid_o = (state_reg == RESP) & i2c_ready;
+
+    // I2C MASTER
+    logic       i2c_done_tick;
+    logic       i2c_ready;
+    logic [7:0] i2c_dout;
+    logic       i2c_ack;
+
+    logic wr_i2c_reg;
+
+    always_ff @(posedge clk_i, i2c_done_tick)
+        if (i2c_done_tick)
+            wr_i2c_reg <= 1'b0;
+        else
+            wr_i2c_reg <= obi_a_fire & obi_awe_i;
+
+    i2c_master i2c(
+        // clk, reset
+        .clk    (clk_i),
+        .reset  (~rstn_i),
+
+        // inputs
+        .wr_i2c (wr_i2c_reg),
+        .cmd    (i2c_data_reg[10:8]),
+        .din    (i2c_data_reg[7:0]),
+        .dvsr   (i2c_speed_reg[15:0]),
+
+        // outputs
+        .dout   (i2c_dout),
+        .ack    (i2c_ack),
+        .ready  (i2c_ready),
+        .done_tick (i2c_done_tick)
+    );
+
+    // I2C RX BUFFER
+    register #(
+        .DTYPE (logic [8:0]),
+        .RESET_VALUE (9'b0)
+    ) i2c_rx_buffer (
+        .clk  (clk_i),
+        .rstn (~rstn_i),
+        .ce   (i2c_done_tick),
+        .in   ({i2c_ack, i2c_dout}),
+        .out  (i2c_status_reg[8:0])
+    );
+
+endmodule
